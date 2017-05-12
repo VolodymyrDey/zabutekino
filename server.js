5235792178bbc3676e96e842aea1546f17946876
Volodymyr Dey <vdey@eagleinvsys.com>
2017-05-12 10:06:07 +0300

    modify package.json

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
